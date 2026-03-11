7123cd0665b08614f7a481dbac5e1be7631fab12
karthikavuri <kavurikarthik402@gmail.com>
2026-03-11 21:41:24 +0530

    signup fix-2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,12 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
-  base: './'
+  base: '/'
 })
